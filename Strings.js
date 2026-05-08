2d4bacdbce300f61e92ee076ae259855f942afb4
Divyesh Bosamiya <bosamiyadivyesh@gmail.com>
2026-05-08 10:14:12 +0530

    Strings

---
+++ Strings.js
@@ -0,0 +1,16 @@
+const name="Divyesh"
+const age=18
+
+console.log(name+" "+age)
+
+console.log(`hello my name is ${name} and my age is ${age}`)//using templatestring 
+
+const getname=new String("Divyesh")//use oop method
+console.log(getname)
+
+console.log(getname.length)
+console.log(getname.toUpperCase())
+console.log(getname.charAt(2))
+console.log(getname.indexOf("y"))
+console.log(getname.substring(0,4))
+console.log(getname.slice(-3))
