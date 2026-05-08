1b72890cca61987377ead7af84c3c9f048657654
Divyesh Bosamiya <bosamiyadivyesh@gmail.com>
2026-05-08 09:39:26 +0530

    first commit

---
+++ Helloword.js
@@ -0,0 +1,29 @@
+//display data in console and types of console.***
+//plese run all comand in html file so it work properly
+
+console.log("hello word")
+
+
+console.log("my age is 18")
+
+console.log("hello")//use normal value print in console
+console.error("word")//use show  msg in error stayle methos (redtext)
+console.warn("divyesh")//use give worning in console in (yellowtext)
+console.table(["Divyesh",18])//show data in table formate
+console.time()//calculate a particular time in console it define start time
+console.timeEnd()//it show time it calculate start  and end and and calculate 
+console.trace()//show function callstack
+console.count("click")//count how maytime calls 
+console.clear()//use clear terminal
+
+console.log(2+"3")
+console.log(2*"3")
+console.log("3"*2)
+
+
+// git init
+// git add .
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/user/repo.git
+// git push -u origin main
---
+++ convertion.js
@@ -0,0 +1,10 @@
+let name="a"
+
+let age=Number(name)
+
+console.table([name,age])
+
+//in javascript for number number keyword use not int use so plese run carefully bro
+//and it not convert single or multiple word in javascript it show Nan(not a number) 
+
+
