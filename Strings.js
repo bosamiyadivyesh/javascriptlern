54b4bd1f27fee478b72d80a952879a21f893f2ab
Divyesh Bosamiya <bosamiyadivyesh@gmail.com>
2026-05-08 13:54:26 +0530

    string functions

--- Strings.js
+++ Strings.js
@@ -8,9 +8,25 @@ console.log(`hello my name is ${name} and my age is ${age}`)//using templatestri
 const getname=new String("Divyesh")//use oop method
 console.log(getname)
 
-console.log(getname.length)
-console.log(getname.toUpperCase())
-console.log(getname.charAt(2))
-console.log(getname.indexOf("y"))
-console.log(getname.substring(0,4))
-console.log(getname.slice(-3))
+console.log(getname.length)//give length
+console.log(getname.toUpperCase())//convert in to upper string
+console.log(getname.charAt(2))//it give word of specific string in specif index
+console.log(getname.indexOf("y"))//it give word index number in specific string 
+console.log(getname.substring(0,4))//it give specific word in string
+console.log(getname.slice(-7)) //use break strings and make new string
+
+const newtext=new String("   MY PLAIN TEXT    ")
+console.log(newtext)
+console.log(newtext.trim())
+
+const url="http://divyesh%20Bosamiya.com"
+console.log(url)
+console.log(url.replace("%20","-"))
+
+
+const data="my-name-is-divyesh"
+console.log(data)
+console.log(`this use to check specific word in this string or not : ${data.includes('divyesh')}`)
+console.log(`it use break-word and make array : ${data.split("-")}`)
+
+
