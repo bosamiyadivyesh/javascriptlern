610508442d1791776ce765f3e53899fa9687c5e6
Divyesh Bosamiya <bosamiyadivyesh@gmail.com>
2026-05-13 23:37:42 +0530

    fullobject detail

---
+++ Functions.js
@@ -0,0 +1,68 @@
+
+//simple function 
+// function displayname(fristname,lastname){
+//   console.log(`Hello ${fristname},${lastname}`)
+// }
+// displayname("Divyesh","Bosamiya")
+
+
+//retuturn function
+// function addition(num1,num2){
+// return num1+num2
+
+// }
+// let result=addition(40,6)
+
+// console.log(result)
+
+
+//arrow function
+
+// let multiplication=(num1,num2)=>{
+    // console.log(num1*num2);
+    // 
+// }
+// 
+// multiplication(1,47)
+
+// let multiplication=(num1,num2)=>{
+// return num1*num2
+// }
+// let rs=Number(multiplication(47,"2"))
+// console.log(rs);
+
+// let multiplication=(num1,num2)=>(num1*num2)    it work similar return if we not write return thwn we can 
+// let rs=multiplication(1,7)                   we can write () inside ()this act as return
+// console.log(rs)
+
+
+
+
+
+//function with array and object
+
+// function hobby(hobby){
+// console.log(hobby)
+// }
+
+// hobby(["sleep","eat"])
+
+
+// function obj(obj1){
+//     console.log(obj1)
+// }
+
+// obj({name:"Divyesh,age:18"})
+
+
+//no we can create IIEF(immediately invoke function)
+
+(function name(){
+    console.log(`divyesh`);
+})();
+
+
+//with parameter
+(function name(val){
+    console.log(val)
+})("Divyesh")
