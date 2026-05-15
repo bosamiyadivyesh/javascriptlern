e2dec86d8b4c103d14e999ef700817a923318e7b
Divyesh Bosamiya <bosamiyadivyesh@gmail.com>
2026-05-15 20:18:42 +0530

    full if else

---
+++ IfelseControlle.js
@@ -0,0 +1,96 @@
+//now hear we ca use if else
+
+// let age=1
+// let name="Divyesh"
+
+
+// if( name === "Divyesh"){
+//     console.log(`welcome sir ${name}`);
+//  ///   return it use for exit to code if i write return then it exit`s the program
+    
+// }
+// else{
+//         console.log(`sorry sir wrong name `);
+// }
+
+// if(name === "Divyesh"){
+//     if(age>=18){
+//         console.log(`hello bhai welcome`);
+        
+//     }
+//     else{
+//         console.log(`less age`);
+        
+//     }
+// }
+// else{
+//     console.log(`wrong name`);
+    
+// }
+
+
+// let balance=5600
+
+// if(balance<=1000){
+//     console.log(`bad salary`);
+    
+// }
+// else if(balance<=5000){
+//     console.log(`medium salary`);
+    
+// }
+// else if(balance<=10000){
+//     console.log(`good salary`);
+    
+// }
+// else{
+//      console.log(`very good  salary`);
+// }
+
+
+
+let day=6 // in this input i not write brerak in 6 key so it exicute defult it not a mistake it for understanding
+switch (day) {
+    case 1:
+        console.log("monday");
+        
+        break;
+    case 2:
+        console.log("tuseday");
+        
+        break;
+    case 3:
+        console.log("wenday");
+        
+        break;
+    case 4:
+        console.log("friday");
+        
+        break;
+    case 5:
+        console.log("satarday");
+        
+        break;
+    case 6 :
+    console.log("sunday")
+    default:
+        console.log("bhai 0 thi 6 sidhi number chalse");
+        
+        break;
+}
+
+//true 
+// true,"any"," ",1,[],{},function name(){}
+
+//false,nan,undefine,null,bigint 0n,0,-0
+
+//and neew opratore nulles (??)
+
+let sem=null ?? 4;
+console.log(sem);
+
+sem = 5 ?? undefined
+
+console.log(sem);
+
+// nules wor only null and undefine it choise only valu if not then it give null or undfine ehich can you give it in real word it uswe mostly
